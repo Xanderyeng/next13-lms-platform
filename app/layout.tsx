899f39a6a9745ae70dea348ee30b8792d415de5e
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2024-01-15 02:11:02 +0300

    Added Vercel Traffic Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/confetti-provider'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           <ToastProvider />
           <SpeedInsights/>
           {children}
+          <Analytics mode={'development'} debug={false} />;
         </body>
       </html>
     </ClerkProvider>
